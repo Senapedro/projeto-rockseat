b31a7a0f70b2c45d7286974696c2b02e4f59c1ee
pedro sena <ph997595305@gmail.com>
2023-10-29 00:08:48 -0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function toggleMode() {
     const html = document.documentElement
     html.classList.toggle('light')
 
-    //const img = document.querySelector("#profile img")
+    const img = document.querySelector("#profile img")
    
 }
